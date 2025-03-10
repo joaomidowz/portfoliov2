7fd792e3f0619d19c9b6f7538ca6b7f60f7fae50
João Gabriel <jgcustodio2005@gmail.com>
2025-03-10 08:12:00 -0300

    Adding Slider and making functional

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,59 +1,14 @@
 "use client"; // Adicione isso no topo do arquiv
-import '../app/swiper.css'
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import Slider from "./Slider";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper/modules';
-import Image from "next/image";
 
 export default function Projects() {
     return (
         <div className="h-100 rounded-t-2xl shadow-3xl bg-secundary font-all font-bold">
             <h2 className="text-base text-primary p-5">Projetos</h2>
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={10}
-                freeMode={true}
-                loop={true}
-                modules={[Pagination, Navigation]}
-                className="ProjectsSwiper"
-            >
-                <SwiperSlide>
-                    <Image
-                        className='Images'
-                        width={228}
-                        height={295}
-                        alt=""
-                        src="/LoginAnimated.svg"
-                        style={{ objectFit: "cover" }}
-                        sizes="" /> </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        className='Images'
-                        width={228}
-                        height={295}
-                        alt=""
-                        src="/Unichat.svg"
-                        sizes="" /> </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        className='Images'
-                        width={228}
-                        height={295}
-                        alt=""
-                        src="/Unichat.svg"
-                        sizes="" /> </SwiperSlide>
-                <SwiperSlide>
-                    <Image
-                        className='Images'
-                        width={228}
-                        height={295}
-                        alt=""
-                        src="/LoginAnimated.svg"
-                        sizes="" /> </SwiperSlide>
-            </Swiper>
+            <div className="">
+            <Slider/>
+            </div>
         </div>
     );
 }
---
+++ components/Slider.tsx
@@ -0,0 +1,61 @@
+"use client";
+import '../app/swiper.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import Image from "next/image";
+
+export default function Slider() {
+    return (
+        <div className="">
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                freeMode={true}
+                loop={true}
+                modules={[Pagination, Navigation]}
+                className="ProjectsSwiper transition-transform duration-300 ease-out hover:scale-105 active:scale-90"
+            >
+                <SwiperSlide>
+                    <Image
+                        className='Images'
+                        width={208}
+                        height={255}
+                        alt=""
+                        src="/LoginAnimated.svg"
+                        style={{ objectFit: "cover" }}
+                        sizes="" /> </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        className='Images '
+                        width={228}
+                        height={295}
+                        alt=""
+                        src="/Unichat.svg"
+                        sizes="" /> </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        className='Images'
+                        width={228}
+                        height={295}
+                        alt=""
+                        src="/Unichat.svg"
+                        sizes="" /> </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        className='Images'
+                        width={228}
+                        height={295}
+                        alt=""
+                        src="/LoginAnimated.svg"
+                        sizes="" /> </SwiperSlide>
+            </Swiper>
+
+        </div>
+    );
+}
+
+
